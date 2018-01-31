0ea8233c497d6f1ef66aa4350b05cbd4fc6756dc
BIT-USER <BIT-USER@DESKTOP-VEEGU4M>
2018-01-31 16:02:52 +0900

    1

---
+++ src/main/java/com/javaex/dao/GuestbookDao.java
@@ -0,0 +1,180 @@
+package com.javaex.dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.stereotype.Repository;
+
+import com.javaex.vo.GuestbookVo;
+
+
+@Repository
+public class GuestbookDao {
+
+
+	public void insert(GuestbookVo vo) {
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+
+		try {
+		    // 1. JDBC 드라이버 (Oracle) 로딩
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+
+		    // 2. Connection 얻어오기
+			String url = "jdbc:oracle:thin:@localhost:1521:xe";
+			conn = DriverManager.getConnection(url, "webdb", "webdb");
+
+		    // 3. SQL문 준비 / 바인딩 / 실행
+		    String query = "insert into guestbook values (seq_guestbook_no.nextval, ? , ? , ? , sysdate )";
+		    pstmt = conn.prepareStatement(query);
+		    
+		    pstmt.setString(1,vo.getName());
+			pstmt.setString(2,vo.getPassword());
+			pstmt.setString(3,vo.getContent());
+			
+			int count = pstmt.executeUpdate();
+		   
+			// 4.결과처리
+			System.out.println(count + "건 저장 완료");
+			
+		} catch (ClassNotFoundException e) {
+		    System.out.println("error: 드라이버 로딩 실패 - " + e);
+		} catch (SQLException e) {
+		    System.out.println("error:" + e);
+		} finally {
+		   
+		    // 5. 자원정리
+		    try {
+		       if (rs != null) {
+		            rs.close();
+		        }             
+		        if (pstmt != null) {
+		            pstmt.close();
+		        }
+		        if (conn != null) {
+		            conn.close();
+		        }
+		    } catch (SQLException e) {
+		        System.out.println("error:" + e);
+		    }
+
+		}
+		
+	
+	}
+	
+	public List<GuestbookVo>getList(){
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		 List<GuestbookVo> gList = new ArrayList<GuestbookVo>();
+		try {
+		    // 1. JDBC 드라이버 (Oracle) 로딩
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+		    // 2. Connection 얻어오기
+			String url = "jdbc:oracle:thin:@localhost:1521:xe";
+			conn = DriverManager.getConnection(url, "webdb", "webdb");
+		    // 3. SQL문 준비 / 바인딩 / 실행
+		    String query = " select no, name , password , content , reg_date "
+		    		+ " from guestbook ";
+		    pstmt = conn.prepareStatement(query);
+		    rs = pstmt.executeQuery();
+	
+		    // 4.결과처리
+		    while(rs.next()) {
+		    	
+		    	GuestbookVo vo = new GuestbookVo();//실제 데이타는 디비안에 있으니까 디비에서 조회해야함 
+		    	
+		    int no = rs.getInt("no");
+		    String name = rs.getString("name");
+		    String password = rs.getString("password");
+		    String content = rs.getString("content");
+		    String date = rs.getString("reg_date");
+		    
+		    vo.setNo(no);
+		    vo.setName(name);
+		    vo.setPassword(password);
+		    vo.setContent(content);
+		    vo.setRegDate(date);
+		    
+		    gList.add(vo);//메모리에만 넣은거니까 add해줘야함 
+		    
+		    
+		   } 
+		    
+		} catch (ClassNotFoundException e) {
+		    System.out.println("error: 드라이버 로딩 실패 - " + e);
+		} catch (SQLException e) {
+		    System.out.println("error:" + e);
+		} finally {
+		   
+		    // 5. 자원정리
+		    try {
+		        if (rs != null) {
+		            rs.close();
+		        }                
+		        if (pstmt != null) {
+		            pstmt.close();
+		        }
+		        if (conn != null) {
+		            conn.close();
+		        }
+		    } catch (SQLException e) {
+		        System.out.println("error:" + e);
+		    }
+
+		}
+		return gList;
+				
+	}	
+	public void delete(int no, String password) {
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		
+		try {
+			// 1. JDBC 드라이버 (Oracle) 로딩
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+
+			// 2. Connection 얻어오기
+			String url = "jdbc:oracle:thin:@localhost:1521:xe";
+			conn = DriverManager.getConnection(url, "webdb", "webdb");
+
+			// 3. SQL문 준비 / 바인딩 / 실행
+			String query = "DELETE FROM guestbook "
+						 + "WHERE no = ? AND password = ?";
+			pstmt = conn.prepareStatement(query);
+			pstmt.setInt(1, no);
+			pstmt.setString(2, password);
+			int result = pstmt.executeUpdate();
+
+			System.out.println("처리결과 : " + result);
+
+		} catch (ClassNotFoundException e) {
+			System.out.println("error: 드라이버 로딩 실패 - " + e);
+		} catch (SQLException e) {
+			System.out.println("error:" + e);
+		} finally {
+			// 5. 자원정리
+			try {
+				if (pstmt != null) {
+					pstmt.close();
+				}
+				if (conn != null) {
+					conn.close();
+				}
+			} catch (SQLException e) {
+				System.out.println("error:" + e);
+			}
+		}
+		
+		
+		
+		
+	}
+}
